36f7aaf4776c210a589b5ec7990abcdcf78523e3
blackfox <blackfox@7a241350-5ad9-0310-a717-93d0c2572c4f>
2004-07-02 03:54:52 +0000

    LGPL Headers

--- trunk/BlackFox.Classes.Xbel/Tests/MetadataProcessor.cs
+++ trunk/BlackFox.Classes.Xbel/Tests/MetadataProcessor.cs
@@ -1,3 +1,22 @@
+/*
+ * Xbel library
+ * Copyright (C) 2004 Roncaglia Julien (Black Fox) <black-fox@virtualblackfox.net>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 using System;
 using System.Xml;
 using NUnit.Framework;
@@ -91,8 +110,8 @@ namespace BlackFox.Classes.Xbel.Tests
 			xmldoc.LoadXml(xmltxt);
 			doc.LoadFromXmlDocument(xmldoc);
 			Assert.AreEqual(doc.Infos.Count, 2, "Il n'y as pas que deux ellements metadata");
-			Assert.IsTrue(doc.Infos[0] is TestProcessor, "Le metadata n'as pas été reconnu comme un \"Testprocessor\"");
-			Assert.IsTrue((doc.Infos[0] as TestProcessor).Test == "Ok", "L'attribut n'as pas été trouvé");
+			Assert.IsTrue(doc.Infos[0] is TestProcessor, "Le metadata n'as pas t reconnu comme un \"Testprocessor\"");
+			Assert.IsTrue((doc.Infos[0] as TestProcessor).Test == "Ok", "L'attribut n'as pas t trouv");
 		}
 		
 		[Test]
@@ -105,7 +124,7 @@ namespace BlackFox.Classes.Xbel.Tests
 			doc.LoadFromXmlDocument(xmldoc);
 			
 			Assert.AreEqual(doc.Infos.Count, 1, "Il n'y as pas qu'un ellement metadata");
-			Assert.IsTrue(doc.Infos[0] is XbelRawMetadata, "Le metadata n'as pas été reconnu comme un \"XbelRawMetadata\"");
+			Assert.IsTrue(doc.Infos[0] is XbelRawMetadata, "Le metadata n'as pas t reconnu comme un \"XbelRawMetadata\"");
 			Assert.IsTrue((doc.Infos[0] as XbelRawMetadata).Content == "<metadata owner=\"void\" param=\"val\">Content</metadata>", "Unexpected Content");
 		}
 		
